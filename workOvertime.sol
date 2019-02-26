ca4e066164de6b91f926ac713b325b43561cacc7
smartct <47882891+smartct@users.noreply.github.com>
2019-02-26 13:20:26 +0900

    0226_v1

--- workOvertime.sol
+++ workOvertime.sol
@@ -1,32 +1,15 @@
 pragma solidity ^0.4.24;
 
-import "./BokkyPooBahsDateTimeLibrary.sol";
 contract work_overtime{
-    using BokkyPooBahsDateTimeLibrary for uint;
-    struct Time{
-        uint hour;
-        uint min;
-        uint sec;
-    }
-    Time startTime;
-    Time endTime;
-    
+
     uint startTimeStamp;
     uint endTimeStamp;
     
     mapping (address => uint256) public balances;
-
-    constructor () public
-    {
-        startTimeStamp = 0;
-        endTimeStamp   = 0;
-    }
+    
     function deposit() public payable returns(bool success) {
         balances[msg.sender] +=msg.value;
         return true;
-    }
-    function showBalanceEth() public view returns(uint256){
-        return balances[msg.sender] / (10**18);
     }
     function withdrawEth(uint value) public returns(bool success) {
         uint valueWei = value * (10**18);
@@ -35,7 +18,11 @@ contract work_overtime{
         msg.sender.transfer(valueWei);
         return true;
     }
-
+    function requestPayment(address to) public returns(bool success){
+        require(startTimeStamp!=0 && endTimeStamp != 0);
+        transferEth(to, 1);
+        success = true;
+    }
     function transferEth(address to, uint value) public returns(bool success) {
         uint valueWei = value * (10**18);
         require(balances[msg.sender] >= valueWei);
@@ -43,48 +30,34 @@ contract work_overtime{
         to.transfer(valueWei);
         return true;
     }
-    
+/*
+    function showBalanceEth() public view returns(uint256){
+        return balances[msg.sender] / (10**18);
+    }
+*/
+
     function setStartTime() public{
         // startTime can be initialized only once
-        if(startTimeStamp == 0 )
-            startTimeStamp = now;
-        
-        (,,,startTime.hour,startTime.min,startTime.sec) =
-            BokkyPooBahsDateTimeLibrary.timestampToDateTime(startTimeStamp);
-            
-        startTime.hour += 9;
+        require( startTimeStamp == 0);
+        startTimeStamp = now;
     }
     function setEndTime() public{
-        require (startTimeStamp != 0);
         // endTime can be initialized whenever you want
+        require (startTimeStamp != 0);
         endTimeStamp = now;
-        (,,,endTime.hour,endTime.min,endTime.sec) =
-            BokkyPooBahsDateTimeLibrary.timestampToDateTime(endTimeStamp);
-            
-        endTime.hour += 9;
     }
-    function requestPayment(address to) public returns(bool success){
-        require(startTimeStamp!=0 && endTimeStamp > startTimeStamp);
-        transferEth(to, 1);
-        success = true;
-    }
-    function getTimeDiff() view public returns(uint hour, uint min, uint sec, bool success){
-        require (startTimeStamp > 0 && endTimeStamp >= startTimeStamp);
-        hour = BokkyPooBahsDateTimeLibrary.diffHours(startTimeStamp, endTimeStamp);
-        min  = BokkyPooBahsDateTimeLibrary.diffMinutes(startTimeStamp, endTimeStamp);
-        sec  = BokkyPooBahsDateTimeLibrary.diffSeconds(startTimeStamp, endTimeStamp);
-        success = true;
+    function initTime() public{
+        startTimeStamp = 0;
+        endTimeStamp = 0;
     }
 
-    function getStartTime() public view returns (uint hour, uint min, uint sec, bool success){
+    function getStartTime() public view returns (uint timeStamp){
         require(startTimeStamp > 0);
-        (hour, min, sec) = (startTime.hour , startTime.min, startTime.sec);
-        success = true;
+        timeStamp = startTimeStamp;
     }
-    function getEndTime() public view returns (uint hour, uint min, uint sec, bool success){
+    function getEndTime() public view returns (uint timeStamp){
         require(endTimeStamp > 0);
-        (hour, min, sec) = (endTime.hour , endTime.min, endTime.sec);
-        success = true;
+        timeStamp = endTimeStamp;
     }
     
 }
